a18b3bb36f27cde26927e93d39b7f35a4f990118
Aly <87067002+Alydiabira@users.noreply.github.com>
2023-01-09 14:53:56 +0100

    Update homepage.php

--- pratique/blog/templates/homepage.php
+++ pratique/blog/templates/homepage.php
@@ -1,7 +1,7 @@
-<?php $title = "Le blog de l'AVBN"; ?>
+<?php $title = "Le blog d' Aly"; ?>
 
 <?php ob_start(); ?>
-<h1>Le super blog de l'AVBN !</h1>
+<h1>Le super blog d'Aly !</h1>
 <p>Derniers billets du blog :</p>
 
 <?php
